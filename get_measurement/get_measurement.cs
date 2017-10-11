bade6d2a996c00ffa3c35e8726b845339b34ab36
RasEza <rasmus.appel@gmail.com>
2017-10-11 15:15:34 +0200

    Håmdtering af forkert ip-adresse formatering

--- get_measurement/get_measurement.cs
+++ get_measurement/get_measurement.cs
@@ -15,53 +15,62 @@ namespace get_measurement
 
 		private get_measurement(string[] args)
 		{
-			IPAddress serverAddress = IPAddress.Parse(args[0]);
-			var udpClient = new UdpClient(PORT);
-			Console.WriteLine("Client started.");
 
-			IPEndPoint serverEP = new IPEndPoint(serverAddress, PORT);
-			Console.WriteLine("Endpoint established.");
-
-			udpClient.Connect(serverEP);
-			Console.WriteLine("Connected to server.");
-
-			Byte[] recData;
-		    if (args[1].ToLower() == "l" || args[1].ToLower() == "u") 
+		    try
 		    {
-		        Byte[] sendLetter = Encoding.ASCII.GetBytes(args[1]);
-		        udpClient.Send(sendLetter, sendLetter.Length);
-		        Console.WriteLine(args[1] + " sendt til server");
+		        IPAddress serverAddress = IPAddress.Parse(args[0]);
 
-                recData = udpClient.Receive(ref serverEP);
-		        Console.WriteLine("Information fra server modtaget:");
-		        Console.WriteLine(Encoding.ASCII.GetString(recData));
-                }
-		    else
-		    {
-		        Console.WriteLine("U or L, you stupido idioti.");
+		        var udpClient = new UdpClient(PORT);
+		        Console.WriteLine("Client started.");
+
+		        IPEndPoint serverEP = new IPEndPoint(serverAddress, PORT);
+		        Console.WriteLine("Endpoint established.");
+
+		        udpClient.Connect(serverEP);
+		        Console.WriteLine("Connected to server.");
+
+		        Byte[] recData;
+		        if (args[1].ToLower() == "l" || args[1].ToLower() == "u")
+		        {
+		            Byte[] sendLetter = Encoding.ASCII.GetBytes(args[1]);
+		            udpClient.Send(sendLetter, sendLetter.Length);
+		            Console.WriteLine(args[1] + " sendt til server");
+
+		            recData = udpClient.Receive(ref serverEP);
+		            Console.WriteLine("Information fra server modtaget:");
+		            Console.WriteLine(Encoding.ASCII.GetString(recData));
+		        }
+		        else
+		        {
+		            Console.WriteLine("U or L, you stupido idioti.");
+		        }
+		        //switch (args[1].ToLower())
+		        //{
+		        //	case "l":
+		        //		Byte[] sendL = Encoding.ASCII.GetBytes(args[1]);
+		        //		udpClient.Send(sendL, sendL.Length);
+		        //		recData = udpClient.Receive(ref serverEP);
+		        //		Console.WriteLine(Encoding.ASCII.GetString(recData));
+		        //		Console.ReadKey();
+		        //		break;
+
+		        //	case "u":
+		        //		Byte[] sendU = Encoding.ASCII.GetBytes(args[1]);
+		        //		udpClient.Send(sendU, sendU.Length);
+		        //		recData = udpClient.Receive(ref serverEP);
+		        //		Console.WriteLine(Encoding.ASCII.GetString(recData));
+		        //		Console.ReadKey();
+		        //		break;
+
+		        //	default:
+		        //		Console.WriteLine("U or L, you stupido idioti.");
+		        //		break;
+		        //}
             }
-            //switch (args[1].ToLower())
-            //{
-            //	case "l":
-            //		Byte[] sendL = Encoding.ASCII.GetBytes(args[1]);
-            //		udpClient.Send(sendL, sendL.Length);
-            //		recData = udpClient.Receive(ref serverEP);
-            //		Console.WriteLine(Encoding.ASCII.GetString(recData));
-            //		Console.ReadKey();
-            //		break;
-
-            //	case "u":
-            //		Byte[] sendU = Encoding.ASCII.GetBytes(args[1]);
-            //		udpClient.Send(sendU, sendU.Length);
-            //		recData = udpClient.Receive(ref serverEP);
-            //		Console.WriteLine(Encoding.ASCII.GetString(recData));
-            //		Console.ReadKey();
-            //		break;
-
-            //	default:
-            //		Console.WriteLine("U or L, you stupido idioti.");
-            //		break;
-            //}
+            catch (System.FormatException)
+		    {
+		        Console.WriteLine($"Fej: lDen indtastede IP adresse {args[0]} har forkert format");
+		    }
         }
 
 		static void Main(string[] args)
