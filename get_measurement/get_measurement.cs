cf99e02e776dbfd03645ac0da759248dc664beaa
Egosheep <jacob_kutten@hotmail.com>
2017-10-11 13:14:12 +0200

    FIXED THE SHIT

---
+++ get_measurement/get_measurement.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace get_measurement
+{
+	class get_measurement
+	{
+		const int PORT = 9000;
+
+		private get_measurement(string[] args)
+		{
+			IPAddress serverAddress = IPAddress.Parse("10.0.0.1");
+			var udpClient = new UdpClient(PORT);
+			Console.WriteLine("Client started.");
+
+			IPEndPoint serverEP = new IPEndPoint(serverAddress, PORT);
+			Console.WriteLine("Endpoint established.");
+
+			udpClient.Connect(serverEP);
+			Console.WriteLine("Connected to server.");
+
+			Byte[] recData;
+			switch (args.ToString().ToLower())
+			{
+				case "l":
+					Byte[] sendL = Encoding.ASCII.GetBytes(args[1]);
+					udpClient.Send(sendL, sendL.Length);
+					recData = udpClient.Receive(ref serverEP);
+					Console.WriteLine(Encoding.ASCII.GetString(recData));
+					Console.ReadKey();
+					break;
+
+				case "u":
+					Byte[] sendU = Encoding.ASCII.GetBytes(args[1]);
+					udpClient.Send(sendU, sendU.Length);
+					recData = udpClient.Receive(ref serverEP);
+					Console.WriteLine(Encoding.ASCII.GetString(recData));
+					Console.ReadKey();
+					break;
+
+				default:
+					Console.WriteLine("U or L, you stupido idioti.");
+					break;
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			new get_measurement(args);
+		}
+	}
+}
---
+++ measurement_server/measurement_server.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace measurement_server
+{
+	class measurement_server
+	{
+		const int PORT = 9000;
+
+		private measurement_server()
+		{
+			//IPAddress localAddress = IPAddress.Parse("10.0.0.1");
+			var udpServer = new UdpClient(PORT);
+			Console.WriteLine("Server started.");
+
+			while (true)
+			{
+				IPEndPoint clientEP = new IPEndPoint(IPAddress.Any, PORT);
+				Console.WriteLine("Client created.");
+				Console.WriteLine("Awaiting input:");
+				Byte[] data = udpServer.Receive(ref clientEP);
+				Console.WriteLine("Recieved: " + data.ToString() + " from client.");
+				//Get choice from client
+				switch (data.ToString().ToLower())
+				{
+
+					case "l":
+						Byte[] sendLoadData = File.ReadAllBytes("/proc/loadavg");
+						udpServer.Send(sendLoadData, sendLoadData.Length);
+						break;
+
+					case "u":
+						Byte[] sendUptimeData = File.ReadAllBytes("/proc/uptime");
+						udpServer.Send(sendUptimeData, sendUptimeData.Length);
+						break;
+
+					default:
+						Console.WriteLine("Received invalid data.");
+						break;
+				}
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			new measurement_server();
+		}
+	}
+}
